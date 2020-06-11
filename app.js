8dcc420be678b4fe0e841b3e8937fb998a7ad5f5
mehdi <mahdinickhoo@gmail.com@users.noreply.github.com>
2020-06-11 15:52:07 +0430

    Chat bot has been pushed

---
+++ app.js
@@ -0,0 +1,23 @@
+
+require('dotenv/config');
+const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+const TelegramBot = require('node-telegram-bot-api');
+const options = {
+  webHook: {
+    port: process.env.PORT
+  }
+};
+
+const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
+
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'Your Chat Id is: ' + msg.chat.id);
+});
